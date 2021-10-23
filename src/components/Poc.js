c332b441f1613c96d5f14e892bc929d22f3f9274
Luis Saturno <saturno.luis@gmail.com>
2021-10-22 23:13:43 -0300

    Add markdown for coverflow and albums

--- src/components/Poc.js
+++ src/components/Poc.js
@@ -1,5 +1,21 @@
+import './Poc.css';
+
 export default function Poc () {
   return (
-    <h1>HOLA</h1>
+    <>
+      <div className="Coverflow">
+        <ul className="Coverflow__wrapper">
+          <li className="Album">1</li>
+          <li className="Album">2</li>
+          <li className="Album">3</li>
+          <li className="Album">4</li>
+          <li className="Album">5</li>
+          <li className="Album">6</li>
+          <li className="Album">7</li>
+          <li className="Album">8</li>
+          <li className="Album">9</li>
+        </ul>
+      </div>
+    </>
   );
 }
